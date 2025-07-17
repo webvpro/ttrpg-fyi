f80684a30341041da3fdbe732d8d995f150555bd
webvpro <mlstanley@gmail.com>
2025-07-17 11:28:41 -0700

    stuff for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel'; // Using the unified adapter
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  output: "server",
+  output: "hybrid", // Using 'hybrid' is recommended with the unified adapter
   adapter: vercel(),
-  integrations: [mdx(), icon({
+  integrations: [
+    mdx(),
+    // prettier-ignore
+    icon({
+      // WARNING: Using a wildcard loads all icons from the set, which can harm performance.
       include: {
         mdi: ["*"],
-        "game-icons": ["*"], // (Default) Loads entire Material Design Icon set
+        "game-icons": ["*"],
       },
-    })],
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
